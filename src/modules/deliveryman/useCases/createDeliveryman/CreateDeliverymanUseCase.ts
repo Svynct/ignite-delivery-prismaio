6b8830435eda98739200c836c0e06f737a21c14d
ricardoamaralcrm <ricardo.crmservices@parceiro.elevaeducacao.com.br>
2022-05-17 22:00:30 -0300

    creating deliveryman route

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanUseCase.ts
@@ -0,0 +1,35 @@
+import { hash } from "bcryptjs";
+
+import { prisma } from "../../../../database/prismaClient";
+
+interface ICreateDeliveryman {
+  username: string;
+  password: string;
+}
+
+export class CreateDeliverymanUseCase {
+  async execute({ username, password }: ICreateDeliveryman) {
+    const clientExist = await prisma.deliveryman.findFirst({
+      where: {
+        username: {
+          mode: "insensitive",
+        }
+      },
+    })
+
+    if (clientExist) {
+      throw new Error("Deliveryman already exists");
+    }
+
+    const hashPassword = await hash(password, 10);
+
+    const client = await prisma.deliveryman.create({
+      data: {
+        username,
+        password: hashPassword
+      }
+    })
+
+    return client;
+  }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -2,13 +2,16 @@ import { Router } from "express";
 
 import { AuthenticateClientController } from "./modules/accounts/authenticateClient/AuthenticateClientController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 
 const routes = Router();
 
 const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
+const createDeliverymanController = new CreateDeliverymanController();
 
 routes.post("/clients", createClientController.handle);
+routes.post("/deliveryman", createDeliverymanController.handle);
 routes.post("/authenticate", authenticateClientController.handle);
 
 export { routes };
